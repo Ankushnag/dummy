90bddcd33fca5a9edcdb1285da13fd8ac2f58249
Ankush Nag <ankushnag01@gmail.com>
2022-10-19 11:36:45 +0530

    initial commit

---
+++ AIFSADO_main/core/accounts/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+
+class User(AbstractUser):
+    is_emailverified = models.BooleanField(default=False)
+
+class UserType(models.Model):
+    user=models.OneToOneField(to=User,on_delete=models.SET_NULL)
+    user_type=models.IntegerField()
+
+class UserProfile(models.Model):
+    user_type=models.ForeignKey(to=UserType,on_delete=models.SET_NULL)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    work_number_1=models.CharField(max_length=10)
+    work_number_2=models.CharField(max_length=10,null=True)
+    cell_number=models.CharField(max_length=10,null=True)
+    is_work_numer_1_valid=models.BooleanField(default=False)
+    typep_of_account=models.IntegerField(null=True)
+    profile_image=models.ImageField(upload_to='profile_pictures',null=True)
+    brokerage_name=models.CharField(max_length=100,null=True)
+    sales_persones_license=models.CharField(max_length=100,null=True)
+    agent_broker_license_title=models.CharField(max_length=100,null=True)
+    create_team=models.BooleanField(null=True)
+    team_name=models.CharField(max_length=50,null=True)
+    languages=models.JSONField()
+    linkedin_url=models.URLField(max_length=200,null=True)
+    twitter_url=models.URLField(max_length=200,null=True)
+    favebook_url=models.URLField(max_length=200,null=True)
+    instagram_url=models.URLField(max_length=200,null=True)
+    youtube=models.URLField(max_length=200,null=True)
+    personal_bio=RichTextField(null=True)
+    is_profilepassword=models.BooleanField(null=True)
+    profile_password=models.CharField(max_length=10,null=True)
+    name_of_business_listing=models.CharField(max_length=100, null=True)
+    business_id=models.CharField(max_length=50,null=True)
+    addition_user=models.CharField(max_length=50)
+    number_user=models.IntegerField(null=True)
+    address_line_1=models.CharField(max_length=100, null=True)
+    address_line_2=models.CharField(max_length=100, null=True)
+    town_id=models.CharField(max_length=100)
+    state_id=models.CharField(max_length=100)
+    zip_code_id=models.CharField(max_length=6)
+
+class User_Log(models.Model):
+    user_type = models.ForeignKey(to=UserType, on_delete=models.SET_NULL)
+    action_type = models.BooleanField(default=False)
+    date_time = 
